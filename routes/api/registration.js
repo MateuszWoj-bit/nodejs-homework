b808124678ea45e342268ef1d97513ca51eb4bc5
Mateusz Wójcikowski <yankes90@gmail.com>
2023-05-13 16:23:54 +0200

    hw6 - nodemailer task

--- routes/api/registration.js
+++ routes/api/registration.js
@@ -8,6 +8,8 @@ const path = require("path");
 const jimp = require("jimp");
 const { v4: uuidv4 } = require("uuid");
 
+const sendMail = require("../nodemailer");
+
 require("dotenv").config();
 const secret = process.env.SECRET;
 const router = express.Router();
@@ -33,36 +35,45 @@ const avatarPatchSchema = Joi.object({
 
 
 router.post("/signup", async (req, res) => {
-  try {    
+  try {
     const { error } = userRegistrationSchema.validate(req.body);
     if (error) {
       return res.status(400).json({ message: error.details[0].message });
     }
 
-    
     const existingUser = await User.findOne({ email: req.body.email });
     if (existingUser) {
       return res.status(409).json({ message: "Email in use" });
     }
 
-    
     const avatarURL = gravatar.url(req.body.email, {
       s: "200",
       r: "pg",
       d: "retro",
     });
 
-       
+    const verificationToken = uuidv4(); 
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const newUser = new User({
       email: req.body.email,
       password: hashedPassword,
       avatarURL: avatarURL,
+      verificationToken: verificationToken, 
+      verify: false,
     });
-    const savedUser = await newUser.save();
 
-    
+    const savedUser = await newUser.save();
+   
+    const verificationLink = `${process.env.BASE_URL}/users/verify/${verificationToken}`;
+    const mailOptions = {
+      to: savedUser.email,
+      subject: "Please verify your email address",
+      html: `Please click this link to verify your email address: <a href="${verificationLink}">${verificationLink}</a>`,
+    };
+    await sendMail(mailOptions);
+
     return res.status(201).json({
+      message: "User created. Please check your email to verify your account.",
       user: {
         email: savedUser.email,
         subscription: savedUser.subscription,
@@ -89,6 +100,10 @@ router.post("/login", async (req, res) => {
     if (!user) {
       return res.status(401).json({ message: "Email or password is wrong" });
     }
+
+     if (!user.isEmailVerified) {
+       return res.status(401).json({ message: "Email is not verified" });
+     }
 
     
     const passwordsMatch = await bcrypt.compare(
@@ -184,6 +199,65 @@ router.patch("/avatars", auth, upload.single("avatar"), async (req, res) => {
     await req.user.save();
 
     return res.status(200).json({ avatarURL: req.user.avatarURL });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.get("/verify/:verificationToken", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      verificationToken: req.params.verificationToken,
+    });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    user.verify = true;
+    user.verificationToken = null;
+    await user.save();
+    return res.status(200).json({ message: "Verification successful" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.post("/verify", async (req, res) => {
+  try {
+    const { error } = Joi.object({
+      email: Joi.string().email().required(),
+    }).validate(req.body);
+
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (user.isVerified) {
+      return res
+        .status(400)
+        .json({ message: "Verification has already been passed" });
+    }
+
+    const verificationToken = uuidv4();
+    user.verificationToken = verificationToken;
+    await user.save();
+
+    const verificationLink = `${process.env.BASE_URL}/users/verify/${verificationToken}`;
+    const mailOptions = {
+      to: user.email,
+      subject: "Please verify your email address",
+      html: `Please click this link to verify your email address: <a href="${verificationLink}">${verificationLink}</a>`,
+    };
+    await sendMail(mailOptions);
+
+    return res.json({ message: "Verification email sent" });
   } catch (err) {
     console.error(err);
     return res.status(500).json({ message: "Internal Server Error" });
